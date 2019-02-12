b26e3baaa8deedfbf5b8181848f7ff3254af8208
Eric Leung <30438439+Eric035@users.noreply.github.com>
2019-02-11 23:55:53 -0500

    Update comp551Mini_project2.py

--- comp551Mini_project2.py
+++ comp551Mini_project2.py
@@ -1,58 +1,45 @@
-# COMP551 PROJECT 2: Group number: 5
-# Student Name: Cheuk Hang Leung (Eric), Donovan Chiazzese, Mohamed Maoui
-# Student ID: 260720788, (Put ur ID's here)
-#----------------------------------------------------------------------------------------------------------#
-#from pathlib import Path
-
-import os, sys
-import numpy as np
-posDirectory = "/Users/ericleung/Desktop/Comp/Comp551/Comp551_Project2/project2Reviews/train/pos"
-negDirectory = "/Users/ericleung/Desktop/Comp/Comp551/Comp551_Project2/project2Reviews/train/neg"
-
-def lowerCaseAndSplit (words):
-    wList = (words.lower()).split(",")
-    return wList
-
-posAdjString = "amazing,alluring,adventurous,amusing,awesome,ambitious,beautiful,bold,brainy,breathtaking,blazing,brazen,cool,cheerful,charming,creative,clever,daring,delightful,dazzling,energetic,elegant,excellent,exceptional,emotional,exuberant,fascinating,fantastic,funny,genius,glorious,great,genuine,happy,honest,helpful,heavenly,hilarious,humorous,hearty,Incredible,inspirational,inspiring,impeccable,ingenious,impressive,innovative,insightful,intense,impartial,imaginative,independent,intuitive,inventive,intellectual,intelligent,Jolly,joyful,jubilant,jovial,joyous,Keen,kind,kindhearted,lively,lovable,lovely,marvellous,majestic,modest,nice,positive,passionate,perfect,phenomenal,quirky,ravishing,reserved,romantic,sweet,stunning,smart,sensational,thrilling,tenacious,talented,upbeat,uplifting,vigorous,valiant,versatile,witty,wise,wonderful,welcoming,warmhearted,inventive,witty,funzesty"
-negAdjString = "Absurd,Arrogant,Boring,bad,intolerant,crazy,Miserly,Patronizing,Vulgar,Crude,Obnoxious,Offensive,Violent,Cryptic"
-AdjString = posAdjString + negAdjString
-
-AdjList = lowerCaseAndSplit(AdjString)          # A list of adjectives
-punctuation = str.maketrans("!,?" , 3 * " ")    # Use this list for stripping punctuations off each review
-
-
-def checkAdjFreq (directoryString):   # A function that takes in a review (.txt file), count the frequencies of each adjective that are in our AdjList appear in that particular review.
-    freqAdjDict = {}
-    for w in AdjList:                       # Loop through the review
-        freqAdjDict[w] = 0
-
+    wordIsPresent = {}
     directory = os.listdir(directoryString)
     directoryPath = os.path.normpath(directoryString)
+    review = 0
     for file in directory:
-        filepath = os.path.join(directoryPath, os.path.normpath(file))       # filepath = directoryPath + filename
+        filepath = os.path.join(directoryPath, os.path.normpath(file))       # Filepath = directoryPath + filename
         content = open(filepath, 'r', encoding='latin-1')
         content = content.read()
-        content.translate(punctuation)          # To remove "," , "!" and "?" from each review.
         wordList = (content.lower()).split()
 
         for word in wordList:
-            if word in freqAdjDict:
-                freqAdjDict[word] += 1
-    return freqAdjDict
+            if word in posReviewsFreqDict:
+                wordIsPresent[word] = 1
 
-posReviewsFreqDict = checkAdjFreq(posDirectory)
-negReviewsFreqDict = checkAdjFreq(negDirectory)
+        sum_Class_1 = 0
+        sum_Class_0 = 0
 
-print (posReviewsFreqDict)
-print (negReviewsFreqDict)
+        for word in wordIsPresent:
+            sum_Class_1 += posReviewsFreqDict[word] / (posReviewsFreqDict[word] + negReviewsFreqDict[word] + 1)
+            sum_Class_0 += negReviewsFreqDict[word] / (posReviewsFreqDict[word] + negReviewsFreqDict[word] + 1)
 
-#----------------------------------------------------------------------------------------------------------#
-numPosReviews = 12500           # Constant
-numNegReviews = 12500           # Constant
+        if sum_Class_1 > sum_Class_0:
+            predicted_values_train[review] = 1
+        else:
+            predicted_values_train[review] = 0
+        review += 1
 
-# TODO: Remove all the punction marks. Count number of exclaimation and question marks.
+    numOfPos = float(0)
+    numOfNeg = float(0)
+    for i in range(0,predicted_values_train.size):
+        if predicted_values_train[i] == 1:
+            numOfPos += 1
+        else:
+            numOfNeg += 1
+    print('number of positive reviews = ', numOfPos)
+    print('number of negative reviews = ', numOfNeg)
 
+    if numOfPos > numOfNeg:
+        accuracy = float(numOfPos/float(12501)) * 100
+        print('That gives us an accuracy of: ', accuracy, '%')
+    else:
+        accuracy = float(numOfNeg/float(12501)) * 100
+        print('That gives us an accuracy of: ', accuracy, '%')
 
-
-
-
+    return predicted_values_train
